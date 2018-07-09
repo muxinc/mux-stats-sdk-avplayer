84fb7a039fb9792eef06f6f537ca769e8a8a7ed5
wtao <wtao@mux.com>
2018-07-09 10:23:41 -0700

    another version fix

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -11,7 +11,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"2.0.2";
+NSString *const MUXSDKPluginVersion = @"0.1.0";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
