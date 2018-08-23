7674bf8b2390d2c88c2cff9a2b05ae856ab969eb
wtao <wtao@mux.com>
2018-08-23 14:53:46 -0700

    build with archive for full bitcode

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -11,7 +11,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"0.1.0";
+NSString *const MUXSDKPluginVersion = @"0.1.1";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
--- update-release-frameworks.sh
+++ update-release-frameworks.sh
@@ -11,12 +11,12 @@ mkdir -p Frameworks/tvOS/simulator
 cd MUXSDKStats
 
 # Build tvOS release SDK
-xcodebuild -configuration Release -target MUXSDKStatsTv -scheme 'MUXSDKStatsTv' SYMROOT=$PWD/tv
+xcodebuild -workspace 'MUXSDKStats.xcworkspace' -configuration Release archive -scheme 'MUXSDKStatsTv' SYMROOT=$PWD/tv
 # Build tvOS simulator SDK
-xcodebuild -configuration Release -target MUXSDKStatsTv -scheme 'MUXSDKStatsTv' -destination 'platform=tvOS Simulator,name=Apple TV' SYMROOT=$PWD/tv
+xcodebuild -workspace 'MUXSDKStats.xcworkspace' -configuration Release -scheme 'MUXSDKStatsTv' -destination 'platform=tvOS Simulator,name=Apple TV' SYMROOT=$PWD/tv
 
 # Prepare the release .framework
-cp -R tv/Release-appletvos/MUXSDKStatsTv.framework tv/MUXSDKStatsTv.framework
+cp -R -L tv/Release-appletvos/MUXSDKStatsTv.framework tv/MUXSDKStatsTv.framework
 cp -R tv/Release-appletvos/MUXSDKStatsTv.framework.dSYM tv/MUXSDKStatsTv.framework.dSYM
 TARGET_TV_BINARY=$PWD/tv/MUXSDKStatsTv.framework/MUXSDKStatsTv
 rm $TARGET_TV_BINARY
@@ -26,12 +26,12 @@ lipo -create tv/Release-appletvos/MUXSDKStatsTv.framework/MUXSDKStatsTv tv/Relea
 
 
 # Build iOS release SDK
-xcodebuild -configuration Release -target MUXSDKStatsTv -scheme 'MUXSDKStats' SYMROOT=$PWD/ios
+xcodebuild -workspace 'MUXSDKStats.xcworkspace' -configuration Release archive -scheme 'MUXSDKStats' SYMROOT=$PWD/ios
 # Build iOS simulator SDK
-xcodebuild -configuration Release -target MUXSDKStatsTv -scheme 'MUXSDKStats' -destination 'platform=iOS Simulator,name=iPhone 7' SYMROOT=$PWD/ios
+xcodebuild -workspace 'MUXSDKStats.xcworkspace' -configuration Release -scheme 'MUXSDKStats' -destination 'platform=iOS Simulator,name=iPhone 7' SYMROOT=$PWD/ios
 
 # Prepare the release .framework
-cp -R ios/Release-iphoneos/MUXSDKStats.framework ios/MUXSDKStats.framework
+cp -R -L ios/Release-iphoneos/MUXSDKStats.framework ios/MUXSDKStats.framework
 cp -R ios/Release-iphoneos/MUXSDKStats.framework.dSYM ios/MUXSDKStats.framework.dSYM
 TARGET_IOS_BINARY=$PWD/ios/MUXSDKStats.framework/MUXSDKStats
 rm $TARGET_IOS_BINARY
@@ -44,7 +44,7 @@ cd ..
 # Copy over tvOS frameworks
 cp -R MUXSDKStats/tv/Release-appletvsimulator/MUXSDKStatsTv.framework Frameworks/tvOS/simulator/MUXSDKStatsTv.framework
 cp -R MUXSDKStats/tv/Release-appletvsimulator/MUXSDKStatsTv.framework.dSYM Frameworks/tvOS/simulator/MUXSDKStatsTv.framework.dSYM
-cp -R MUXSDKStats/tv/Release-appletvos/MUXSDKStatsTv.framework Frameworks/tvOS/release/MUXSDKStatsTv.framework
+cp -R -L MUXSDKStats/tv/Release-appletvos/MUXSDKStatsTv.framework Frameworks/tvOS/release/MUXSDKStatsTv.framework
 cp -R MUXSDKStats/tv/Release-appletvos/MUXSDKStatsTv.framework.dSYM Frameworks/tvOS/release/MUXSDKStatsTv.framework.dSYM
 cp -R MUXSDKStats/tv/MUXSDKStatsTv.framework Frameworks/tvOS/fat/MUXSDKStatsTv.framework
 cp -R MUXSDKStats/tv/MUXSDKStatsTv.framework.dSYM Frameworks/tvOS/fat/MUXSDKStatsTv.framework.dSYM
@@ -52,7 +52,7 @@ cp -R MUXSDKStats/tv/MUXSDKStatsTv.framework.dSYM Frameworks/tvOS/fat/MUXSDKStat
 # Copy over iOS frameworks
 cp -R MUXSDKStats/ios/Release-iphonesimulator/MUXSDKStats.framework Frameworks/iOS/simulator/MUXSDKStats.framework
 cp -R MUXSDKStats/ios/Release-iphonesimulator/MUXSDKStats.framework.dSYM Frameworks/iOS/simulator/MUXSDKStats.framework.dSYM
-cp -R MUXSDKStats/ios/Release-iphoneos/MUXSDKStats.framework Frameworks/iOS/release/MUXSDKStats.framework
+cp -R -L MUXSDKStats/ios/Release-iphoneos/MUXSDKStats.framework Frameworks/iOS/release/MUXSDKStats.framework
 cp -R MUXSDKStats/ios/Release-iphoneos/MUXSDKStats.framework.dSYM Frameworks/iOS/release/MUXSDKStats.framework.dSYM
 cp -R MUXSDKStats/ios/MUXSDKStats.framework Frameworks/iOS/fat/MUXSDKStats.framework
 cp -R MUXSDKStats/ios/MUXSDKStats.framework.dSYM Frameworks/iOS/fat/MUXSDKStats.framework.dSYM
