372a91215cbb71a7afec3a169a836a4a8c61c5f7
wtao <wtao@mux.com>
2018-08-28 08:58:07 -0700

    Add ima sdk showcase

--- apps/DemoApp/DemoApp/ViewController.h
+++ apps/DemoApp/DemoApp/ViewController.h
@@ -2,12 +2,18 @@
 
 @import AVKit;
 @import AVFoundation;
+@import GoogleInteractiveMediaAds;
 
-@interface ViewController : UIViewController {
+@interface ViewController : UIViewController <IMAAdsLoaderDelegate, IMAAdsManagerDelegate> {
     @private
     AVPlayer *_avplayer;
     AVPlayerViewController *_avplayerController;
     NSTimer *_videoChangeTimer;
+
+    // IMA SDK variables
+    IMAAdsLoader *_adsLoader;
+    IMAAdsManager *_adsManager;
+    IMAAVPlayerContentPlayhead *_contentPlayhead;
 }
 
 @end
