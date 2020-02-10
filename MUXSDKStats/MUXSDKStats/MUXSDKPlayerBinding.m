0e48c5493951343c661333cfaecd0fe0c03fb75a
Nidhi Kulkarni <nkulkarni@mux.com>
2020-02-10 12:15:30 -0800

    Bump version to 1.1.4

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -6,7 +6,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"1.1.3";
+NSString *const MUXSDKPluginVersion = @"1.1.4";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
