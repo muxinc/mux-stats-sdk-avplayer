b8833e1a2d523439a9cd048f39e3021c733d5029
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2020-02-20 12:32:24 -0800

    fix request throughput metrics - bump to 1.2.1
    * set event.type on MUXSDKRequestBandwidthEvent so that Mux-Core can track it properly
    * new build Frameworks
    * bump to version 1.2.1 for throughput bugfix

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -6,7 +6,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"1.2.0";
+NSString *const MUXSDKPluginVersion = @"1.2.1";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
@@ -133,7 +133,7 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
                 loadData.requestVideoWidth = nil;
                 loadData.requestVideoHeight = nil;
                 loadData.requestRenditionLists = nil;
-                [self dispatchBandwidthMetric:loadData];
+                [self dispatchBandwidthMetric:loadData withType:MUXSDKPlaybackEventRequestBandwidthEventCompleteType];
                 _lastTransferredBytes = event.numberOfBytesTransferred;
                 _lastTransferDuration = event.transferDuration;
             }
@@ -153,7 +153,7 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
                 loadData.requestHostName = [self getHostName:errorEvent.URI];
                 loadData.requestErrorCode = [NSNumber numberWithLong: errorEvent.errorStatusCode];
                 loadData.requestErrorText = errorEvent.errorComment;
-                [self dispatchBandwidthMetric:loadData];
+                [self dispatchBandwidthMetric:loadData withType:MUXSDKPlaybackEventRequestBandwidthEventErrorType];
             }
             _lastErrorLogEventCount = error.events.count;
         }
@@ -588,13 +588,14 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
     _state = MUXSDKPlayerStateViewEnd;
 }
 
-- (void)dispatchBandwidthMetric: (MUXSDKBandwidthMetricData *)loadData {
+- (void)dispatchBandwidthMetric: (MUXSDKBandwidthMetricData *)loadData withType:(NSString *)type {
     if (![self isPlayerOK]) {
         return;
     }
     [self checkVideoData];
     MUXSDKPlayerData *playerData = [self getPlayerData];
     MUXSDKRequestBandwidthEvent *event = [[MUXSDKRequestBandwidthEvent alloc] init];
+    event.type = type;
     [event setPlayerData:playerData];
     [event setBandwidthMetricData: loadData];
     [MUXSDKCore dispatchEvent:event forPlayer:_name];
