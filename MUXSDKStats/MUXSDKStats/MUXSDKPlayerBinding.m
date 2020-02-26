5739c033cebe094e3139103c0638a54adf8c4818
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-02-26 14:15:01 -0800

    patch version bump to 1.3.1

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -7,7 +7,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"1.3.0";
+NSString *const MUXSDKPluginVersion = @"1.3.1";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
--- Mux-Stats-AVPlayer.podspec
+++ Mux-Stats-AVPlayer.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   s.name             = 'Mux-Stats-AVPlayer'
 
-  s.version          = '1.3.0'
+  s.version          = '1.3.1'
   s.source           = { :git => 'https://github.com/muxinc/mux-stats-sdk-avplayer.git',
                          :tag => "v#{s.version}" }
 
