edf0d6e8e45d4b16e3503309a79b455a0187bf10
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-04-08 17:50:24 -0700

    add google IMA ads example in swift demo app

--- apps/video-demo/video-demo/VideoPlayerController.swift
+++ apps/video-demo/video-demo/VideoPlayerController.swift
@@ -3,33 +3,49 @@ import AVKit
 import AVFoundation
 import MuxCore
 import MUXSDKStats
+import GoogleInteractiveMediaAds
+import Mux_Stats_Google_IMA
 
-class VideoPlayerController: AVPlayerViewController {
+class VideoPlayerController: AVPlayerViewController, IMAAdsLoaderDelegate, IMAAdsManagerDelegate {
     var video: Dictionary<String, String>! = nil
     var timeObserverToken: Any! = nil
     var playComplete: Any! = nil
     var controller: AVPlayerViewController! = nil
     var url: URL! = nil
+    var contentPlayhead: IMAContentPlayhead! = nil
+    var adsLoader: IMAAdsLoader! = nil
+    var adsManager: IMAAdsManager! = nil
+    var imaListener: MuxImaListener! = nil
 
     let playName = "iOS AVPlayer"
+    let adUrl = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator="
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let videoUrl = video["url"]!
         url = URL(string: videoUrl)
 //        player = self.testAvPlayer()
-        player = self.testAvQueuePlayer()
+//        player = self.testVideoChange()
+//        player = self.testAvQueuePlayer()
+        player = self.testImaSDK()
 
         let playerData = MUXSDKCustomerPlayerData(environmentKey: "ENV_KEY");
         playerData?.playerName = "AVPlayer"
         let videoData = MUXSDKCustomerVideoData();
         videoData.videoIsLive = false;
         videoData.videoTitle = "Title1"
-        MUXSDKStats.monitorAVPlayerViewController(self, withPlayerName: playName, playerData: playerData!, videoData: videoData);
+        let playerBinding = MUXSDKStats.monitorAVPlayerViewController(self, withPlayerName: playName, playerData: playerData!, videoData: videoData);
+        imaListener = MuxImaListener.init(playerBinding: playerBinding!)
         player!.play()
     }
 
     func testAvPlayer () -> AVPlayer {
+        let item = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: item)
+        return player!
+    }
+
+    func testVideoChange () -> AVPlayer {
         let item1 = AVPlayerItem(url: url!)
         let item2 = AVPlayerItem(url: URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!)
         player = AVPlayer(playerItem: item1)
@@ -58,6 +74,28 @@ class VideoPlayerController: AVPlayerViewController {
         return player!
     }
 
+    func testImaSDK () -> AVPlayer {
+        let item = AVPlayerItem(url: URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!)
+        player = AVPlayer(playerItem: item)
+        contentPlayhead = IMAAVPlayerContentPlayhead.init(avPlayer: player!)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.contentDidFinishPlaying),
+            name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+            object: item
+        )
+        adsLoader = IMAAdsLoader.init()
+        adsLoader.delegate = self;
+        let adDisplayContainer = IMAAdDisplayContainer.init(adContainer: self.view)
+        let request = IMAAdsRequest.init(
+            adTagUrl: adUrl,
+            adDisplayContainer: adDisplayContainer,
+            contentPlayhead: (contentPlayhead as! IMAContentPlayhead & NSObjectProtocol),
+            userContext: nil)
+        adsLoader.requestAds(with: request)
+        return player!
+    }
+
     @objc func playerItemDidReachEnd (notification: NSNotification) {
         let videoData = MUXSDKCustomerVideoData();
         videoData.videoTitle = "Title2"
@@ -65,13 +103,15 @@ class VideoPlayerController: AVPlayerViewController {
         MUXSDKStats.videoChange(forPlayer: self.playName, with: videoData)
     }
 
+    @objc func contentDidFinishPlaying (notification: NSNotification) {
+        if ((notification.object as! AVPlayerItem) == player!.currentItem) {
+            adsLoader.contentComplete()
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    @objc func playerDidFinishPlaying(note: NSNotification) {
-        self.navigationController?.popViewController(animated: true)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -84,5 +124,54 @@ class VideoPlayerController: AVPlayerViewController {
         NotificationCenter.default.removeObserver(playComplete)
         MUXSDKStats.destroyPlayer(playName);
         player = nil
+    }
+
+    // pragma mark - IMAAdsLoaderDelegate
+    func adsLoader(_ loader: IMAAdsLoader!, adsLoadedWith adsLoadedData: IMAAdsLoadedData!) {
+        adsManager = adsLoadedData.adsManager;
+        adsManager.delegate = self;
+        let adsRenderingSettings = IMAAdsRenderingSettings.init()
+        adsRenderingSettings.webOpenerPresentingController = self;
+        adsManager.initialize(with: adsRenderingSettings)
+    }
+
+    func adsLoader(_ loader: IMAAdsLoader!, failedWith adErrorData: IMAAdLoadingErrorData!) {
+        print("Error loading ads: \(String(describing: adErrorData.adError.message))")
+        player!.play()
+    }
+
+    func adsManager(_ adsManager: IMAAdsManager!, didReceive event: IMAAdEvent!) {
+        if (event.type == IMAAdEventType.LOADED) {
+            adsManager.start()
+        }
+        if (imaListener != nil) {
+            imaListener.dispatchEvent(event)
+        }
+    }
+
+    func adsManager(_ adsManager: IMAAdsManager!, didReceive error: IMAAdError!) {
+      // Something went wrong with the ads manager after ads were loaded. Log the
+      // error and play the content.
+        NSLog("AdsManager error: \(String(describing: error.message))")
+        player!.play()
+        if (imaListener != nil) {
+            imaListener.dispatchError(error.message)
+        }
+    }
+
+    func adsManagerDidRequestContentPause(_ adsManager: IMAAdsManager!) {
+      // The SDK is going to play ads, so pause the content.
+        player!.pause()
+        if (imaListener != nil) {
+            imaListener.onContentPauseOrResume(true)
+        }
+    }
+
+    func adsManagerDidRequestContentResume(_ adsManager: IMAAdsManager!) {
+      // The SDK is done playing ads (at least for now), so resume the content.
+        player!.play()
+        if (imaListener != nil) {
+            imaListener.onContentPauseOrResume(false)
+        }
     }
 }
