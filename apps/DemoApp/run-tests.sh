ebe13b9de28416daac1f43c9184ac1cfde6173b3
Nidhi Kulkarni <nkulkarni@mux.com>
2020-10-27 20:01:09 -0400

    split tests into separate step

---
+++ apps/DemoApp/run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+buildkite-agent artifact download "Frameworks/**/*" "../../Frameworks" --step "buildkite.sh"
+pod deintegrate && pod install
+xcodebuild -workspace DemoApp.xcworkspace \
+           -scheme "DemoApp" \
+           -destination 'platform=iOS Simulator,name=iPhone 11,OS=14.1' \
+           test
