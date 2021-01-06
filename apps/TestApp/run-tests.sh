fcaea33c47b67a5b46ae4e3f9063c1c7bbd293e6
Nidhi Kulkarni <nkulkarni@mux.com>
2021-01-05 19:22:20 -0500

    update run tests script

--- apps/TestApp/run-tests.sh
+++ apps/TestApp/run-tests.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # Delete the old stuff
 rm -Rf Frameworks
 
-buildkite-agent artifact download "MUXSDKStats.framework.zip" . --step ".buildkite/build.sh"
-unzip MUXSDKStats.framework.zip
+buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
+unzip MUXSDKStats.xcframework.zip
 cd apps/TestApp
 pod deintegrate && pod install
 xcodebuild -workspace TestApp.xcworkspace \
