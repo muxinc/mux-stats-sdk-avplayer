c8f225fd5553fb551c228416572e87378dce40f2
Andrew Barba <barba@hey.com>
2021-03-17 22:32:13 -0400

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MUXSDKStats",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "MUXSDKStats", targets: ["MUXSDKStats"])
+    ],
+    dependencies: [
+        .package(
+            name: "Mux-Stats-Core",
+            url: "https://github.com/AndrewBarba/stats-sdk-objc.git",
+            .branch("master")
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MUXSDKStats",
+            path: "XCFramework/MUXSDKStats.xcframework"
+        )
+    ]
+)
