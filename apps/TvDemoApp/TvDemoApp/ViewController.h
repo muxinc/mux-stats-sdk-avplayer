acdd29e3b9a6ed80f076ed672da41973933c301d
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-03-19 15:48:56 -0700

    DemoApps Google IMA (#70)

--- apps/TvDemoApp/TvDemoApp/ViewController.h
+++ apps/TvDemoApp/TvDemoApp/ViewController.h
@@ -6,7 +6,7 @@
 @import GoogleInteractiveMediaAds;
 @import MuxCore;
 @import MUXSDKStats;
-//@import Mux_Stats_Google_IMA;
+@import Mux_Stats_Google_IMA;
 
 @interface ViewController : UIViewController <IMAAdsLoaderDelegate, IMAAdsManagerDelegate> {
 @private
@@ -17,7 +17,7 @@
     IMAAdsLoader *_adsLoader;
     IMAAdsManager *_adsManager;
     IMAAVPlayerContentPlayhead *_contentPlayhead;
-//    MuxImaListener *_imaListener;
+    MuxImaListener *_imaListener;
     MUXSDKPlayerBinding *_playerBinding;
 }
 
