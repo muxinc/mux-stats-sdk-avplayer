d7305e5741692c0b1925e99153b82d24ebe85b03
Andrew Barba <barba@hey.com>
2021-03-30 16:19:28 -0400

    SwiftPM Support (#84)

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MUXSDKStats",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "MUXSDKStats", targets: ["MUXSDKStatsTargets"])
+    ],
+    dependencies: [
+        .package(
+            name: "MuxCore",
+            url: "https://github.com/muxinc/stats-sdk-objc.git",
+            from: "3.1.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MUXSDKStats",
+            path: "XCFramework/MUXSDKStats.xcframework"
+        ),
+        .target(
+            name: "MUXSDKStatsTargets",
+            dependencies: [
+                "MuxCore",
+                .target(name: "MUXSDKStats")
+            ],
+            path: "SwiftPM"
+        )
+    ]
+)
