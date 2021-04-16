5f1b1be5b152fad957748b0e27d3c0660f218fcb
Nidhi Kulkarni <nkulkarni@mux.com>
2021-04-16 12:28:07 -0400

    reset simulators before tests run

--- apps/DemoApp/run-tests.sh
+++ apps/DemoApp/run-tests.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 # Delete the old stuff
 rm -Rf XCFramework
-
+# reset simulators
+xcrun simctl shutdown all
+xcrun simctl erase all
 buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
 unzip MUXSDKStats.xcframework.zip
 cd apps/DemoApp
