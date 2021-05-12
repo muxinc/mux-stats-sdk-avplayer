befe65b193efc03857f4b6d776a34853f326f71d
Nidhi Kulkarni <nidhikulkarni82@gmail.com>
2021-05-11 20:03:04 -0400

    Update .buildkite/build-static.sh
    Co-authored-by: Scott Kidder <skidder@users.noreply.github.com>

--- .buildkite/build-static.sh
+++ .buildkite/build-static.sh
@@ -6,4 +6,4 @@ pod repo update
 pod deintegrate && pod install
 cd ..
 ./update-release-xcframeworks-static.sh
-zip -ry MUXSDKStats.xcframework.zip XCFramework
+zip -ry MUXSDKStats-static.xcframework.zip XCFramework
