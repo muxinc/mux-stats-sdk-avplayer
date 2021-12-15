e31d312c040bc34fe8305952eb7f1e0c934a615f
Stephanie Zuniga <szuniga@konradgroup.com>
2021-12-15 10:10:08 -0600

    v2.8.0

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -12,7 +12,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"2.7.0";
+NSString *const MUXSDKPluginVersion = @"2.8.0";
 
 // Min number of seconds between timeupdate events. (100ms)
 double MUXSDKMaxSecsBetweenTimeUpdate = 0.1;
