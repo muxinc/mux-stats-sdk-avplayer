c7f01f489a96b6daa331efb2adb6b3705f787410
Mike English <93936348+englishm-mux@users.noreply.github.com>
2022-01-20 12:30:30 -0500

    Remove buildkite-agent dep for fetching artifact
    Addresses:
    https://github.com/englishm-mux/mux-stats-sdk-avplayer/runs/4885955788?check_suite_focus=true#step:5:24

--- apps/DemoApp/run-tests.sh
+++ apps/DemoApp/run-tests.sh
@@ -6,7 +6,6 @@ rm -Rf XCFramework
 # reset simulators
 xcrun -v simctl shutdown all
 xcrun -v simctl erase all
-buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
 unzip MUXSDKStats.xcframework.zip
 cd apps/DemoApp
 pod deintegrate && pod update
