36ed8da001e4dd1b3d60ae84bb7f42ef4e3e941a
Nidhi Kulkarni <nkulkarni@mux.com>
2022-01-20 18:52:16 -0500

    TEst on big sur with newer sims

---
+++ .buildkite/unit-tests-big-sur.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+cd MUXSDKStats
+pod repo update
+pod deintegrate && pod install
+cd ..
+PROJECT=MUXSDKStats/MUXSDKStats.xcworkspace
+
+xcodebuild clean test \
+  -workspace $PROJECT \
+  -scheme MUXSDKStats \
+  -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.2' \
