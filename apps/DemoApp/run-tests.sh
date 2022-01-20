0af15ad50b7a102d152417a2684dc3eeafc9e482
Mike English <menglish@mux.com>
2022-01-20 15:39:12 -0500

    Re-add support for running in buildkite
    This should make the transition easier by allowing buildkite jobs to
    continue running as-is while we ensure feature parity with GitHub Actions

--- apps/DemoApp/run-tests.sh
+++ apps/DemoApp/run-tests.sh
@@ -6,6 +6,13 @@ rm -Rf XCFramework
 # reset simulators
 xcrun -v simctl shutdown all
 xcrun -v simctl erase all
+
+# Fetch artifact if running via buildkite (GitHub Actions fetches the artifact in a prior step)
+if command -v buildkit-agent > /dev/null 2>&1;
+then
+    buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
+fi
+
 unzip MUXSDKStats.xcframework.zip
 cd apps/DemoApp
 pod deintegrate && pod update
