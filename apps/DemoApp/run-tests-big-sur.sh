9a3067c222b7c2c33f0a2cd01356ae53572dbabc
Nidhi Kulkarni <nkulkarni@mux.com>
2022-01-20 19:25:22 -0500

    it has the same name I think

--- apps/DemoApp/run-tests-big-sur.sh
+++ apps/DemoApp/run-tests-big-sur.sh
@@ -13,7 +13,7 @@ then
     buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
 fi
 
-unzip MUXSDKStats-Big-Sur.xcframework.zip
+unzip MUXSDKStats.xcframework.zip
 cd apps/DemoApp
 pod deintegrate && pod update
 xcodebuild -workspace DemoApp.xcworkspace \
