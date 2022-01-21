ee89375c4d0e758cfe843a7365ba696fd5661842
Nidhi Kulkarni <nidhikulkarni82@gmail.com>
2022-01-20 21:34:36 -0500

    Add XCUITest job for Big Sur, iPhone 12 & iOS 15.0 simulators (#141)
    Co-authored-by: Nidhi Kulkarni <nkulkarni@mux.com>

---
+++ apps/DemoApp/run-tests-big-sur.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# Delete the old stuff
+rm -Rf XCFramework
+# reset simulators
+xcrun -v simctl shutdown all
+xcrun -v simctl erase all
+
+# Fetch artifact if running via buildkite (GitHub Actions fetches the artifact in a prior step)
+if command -v buildkite-agent > /dev/null 2>&1;
+then
+    buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
+fi
+
+unzip MUXSDKStats.xcframework.zip
+cd apps/DemoApp
+pod deintegrate && pod update
+xcodebuild -workspace DemoApp.xcworkspace \
+           -scheme "DemoApp" \
+           -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.0' \
+           test
