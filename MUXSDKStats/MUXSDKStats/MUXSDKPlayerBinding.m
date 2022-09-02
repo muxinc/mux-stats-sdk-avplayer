1d11e1778263a92fba50b3b2fae12435abd610a8
Emily Dixon <edixon@mux.com>
2022-09-02 10:55:21 -0700

    Fix Error Metadata being reported, but no Error Events (#161)

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -1014,7 +1014,8 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
     if (!_player || !_playerItem) {
         return NO;
     }
-    return _player.status == AVPlayerStatusFailed || _playerItem.status == AVPlayerItemStatusFailed;
+    // check for presence of errors rather than player status
+    return _player.error || _playerItem.error;
 }
 
 - (void)observeValueForKeyPath:(NSString*) path
