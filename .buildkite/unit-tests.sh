c1f3192459631a087a26530c545501fbfdbb18a4
Emily Dixon <edixon@mux.com>
2022-10-18 14:10:24 -0700

    Update some buildkite stuff, see if it works

--- .buildkite/unit-tests.sh
+++ .buildkite/unit-tests.sh
@@ -10,4 +10,4 @@ PROJECT=MUXSDKStats/MUXSDKStats.xcworkspace
 xcodebuild clean test \
   -workspace $PROJECT \
   -scheme MUXSDKStats \
-  -destination 'platform=iOS Simulator,name=iPhone 11,OS=14.1' \
+  -destination 'platform=iOS Simulator,name=iPhone 13 Pro,OS=15.5' 
