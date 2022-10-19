efe302929567d33abc8f49c5280783e8b2871ea4
Emily Dixon <edixon@mux.com>
2022-10-18 18:32:04 -0700

    Remove buildkite pipeline

--- .buildkite/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-cd MUXSDKStats
-pod repo update
-pod deintegrate && pod install
-cd ..
-./update-release-xcframeworks.sh
-zip -ry MUXSDKStats.xcframework.zip XCFramework
