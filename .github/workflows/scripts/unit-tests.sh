775a2983bf1af8f197a9f524408d2025aa8e76bd
Emily Dixon <edixon@mux.com>
2022-10-18 19:20:33 -0700

    Try another simulator

--- .github/workflows/scripts/unit-tests.sh
+++ .github/workflows/scripts/unit-tests.sh
@@ -10,4 +10,4 @@ PROJECT=MUXSDKStats/MUXSDKStats.xcworkspace
 xcodebuild clean test \
   -workspace $PROJECT \
   -scheme MUXSDKStats \
-  -destination 'platform:iOS Simulator, id:020403AC-9B29-406D-93A3-06980FBD6750, OS:14.4, name:iPhone 12 Pro Max' 
+  -destination 'platform:iOS Simulator, OS:14.4, name:iPhone 12 Pro Max' 
