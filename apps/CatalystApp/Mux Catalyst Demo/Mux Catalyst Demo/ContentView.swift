553d2c195904e05377e3980f58d493342b4a8aa1
Emily Dixon <edixon@mux.com>
2022-10-21 12:28:14 -0700

    Initial commit of Catalyst Demo

---
+++ apps/CatalystApp/Mux Catalyst Demo/Mux Catalyst Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mux Catalyst Demo
+//
+//  Created by Emily Dixon on 10/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apps/CatalystApp/Mux Catalyst Demo/Mux Catalyst Demo/Mux_Catalyst_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mux_Catalyst_DemoApp.swift
+//  Mux Catalyst Demo
+//
+//  Created by Emily Dixon on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Mux_Catalyst_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
