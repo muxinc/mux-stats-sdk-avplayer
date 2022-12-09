c28d501c9722b4b4221c6f527e940b60881c7b39
Emily Dixon <edixon@mux.com>
2022-12-09 15:56:46 -0800

    More version bumps + Build XCFramework

--- update-release-xcframeworks-static.sh
+++ update-release-xcframeworks-static.sh
@@ -20,7 +20,8 @@ BUILD_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
  xcodebuild archive -scheme MUXSDKStats -workspace $PROJECT  -destination "generic/platform=iOS" -archivePath "$BUILD_DIR/MUXSDKStats.iOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIB\
 UTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
  xcodebuild archive -scheme MUXSDKStats -workspace $PROJECT  -destination "generic/platform=iOS Simulator" -archivePath "$BUILD_DIR/MUXSDKStats.iOS-simulator.xcarchive" SKIP_INSTALL=NO BUILD\
-_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
+_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib \
+| xcbeautify
 
   xcodebuild archive -scheme MUXSDKStats -workspace $PROJECT  -destination "generic/platform=macOS,variant=Mac Catalyst" -archivePath "$BUILD_DIR/MUXSDKStats.macOS.xcarchive" SKIP_INSTALL=NO\
  BUILD_LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib \
@@ -31,6 +32,7 @@ _LIBRARY_FOR_DISTRIBUTION=YES CLANG_ENABLE_MODULES=NO MACH_O_TYPE=staticlib
                                 -framework "$BUILD_DIR/MUXSDKStats.iOS.xcarchive/Products/Library/Frameworks/MUXSDKStats.framework" \
                                 -framework "$BUILD_DIR/MUXSDKStats.iOS-simulator.xcarchive/Products/Library/Frameworks/MUXSDKStats.framework" \
                                 -framework "$BUILD_DIR/MUXSDKStats.macOS.xcarchive/Products/Library/Frameworks/MUXSDKStats.framework" \
-                                -output "$TARGET_DIR/MUXSDKStats.xcframework"
+                                -output "$TARGET_DIR/MUXSDKStats.xcframework" \
+| xcbeautify
 
 rm -Rf $BUILD_DIR
