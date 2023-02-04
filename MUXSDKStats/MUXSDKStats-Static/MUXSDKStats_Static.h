0ca38759cd4a4732658517d8184c1450efb61a02
Emily Dixon <edixon@mux.com>
2023-02-03 16:19:24 -0800

    Explicitly add a statically-linked target

---
+++ MUXSDKStats/MUXSDKStats-Static/MUXSDKStats_Static.h
@@ -0,0 +1,19 @@
+//
+//  MUXSDKStats_Static.h
+//  MUXSDKStats-Static
+//
+//  Created by Emily Dixon on 2/3/23.
+//  Copyright © 2023 Mux, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MUXSDKStats_Static.
+FOUNDATION_EXPORT double MUXSDKStats_StaticVersionNumber;
+
+//! Project version string for MUXSDKStats_Static.
+FOUNDATION_EXPORT const unsigned char MUXSDKStats_StaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MUXSDKStats_Static/PublicHeader.h>
+
+
