7f2fa1818f41bb286d450c0374ed26dc74324f24
Emily Dixon <edixon@mux.com>
2023-03-30 14:15:05 -0700

    Version bumps

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -12,7 +12,7 @@
 
 // SDK constants.
 NSString *const MUXSDKPluginName = @"apple-mux";
-NSString *const MUXSDKPluginVersion = @"3.2.0";
+NSString *const MUXSDKPluginVersion = @"3.2.1";
 NSString *const MUXSessionDataPrefix = @"io.litix.data.";
 
 // Min number of seconds between timeupdate events. (100ms)
