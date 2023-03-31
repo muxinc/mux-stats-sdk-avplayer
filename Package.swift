512269db8a64d941ff44a9c4ae4c39b419c8b65b
Emily Dixon <edixon@mux.com>
2023-03-30 17:32:18 -0700

    Releases/v3.2.1 (#182)
    ## Fixes
    
    * Fix Ad metadata not showing up correctly when dispatched on ad events
    
    Co-authored-by: Emily Dixon <edixon@mux.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(
             name: "MuxCore",
             url: "https://github.com/muxinc/stats-sdk-objc.git",
-            from: "4.4.0"
+            from: "4.4.2"
         )
     ],
     targets: [
