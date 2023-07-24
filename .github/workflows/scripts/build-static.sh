7b0e6e2eda1298e145264c5c4e8663d0f8e807d3
AJ Lauer Barinov <102617203+andrewjl-mux@users.noreply.github.com>
2023-07-24 14:45:52 -0700

    Releases/v3.3.1 (#186)
    * Backfill header nullability annotations to remove build warnings (#185)

--- .github/workflows/scripts/build-static.sh
+++ .github/workflows/scripts/build-static.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 
 cd MUXSDKStats
+pod cache clean --all
 pod repo update
-pod deintegrate && pod install
+pod deintegrate && pod install --repo-update
 cd ..
 ./update-release-xcframeworks-static.sh
 zip -ry MUXSDKStats-static.xcframework.zip XCFramework
