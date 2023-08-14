a9a80de33a0266dfaa56f6c06fb9188167887f29
AJ Lauer Barinov <abarinov@mux.com>
2023-08-14 14:10:55 -0700

    ci: update iOS Simulator and macOS versions (#188)
    Match simulator version to Xcode 13.4.1 version when running tests
    
    Use Xcode 13.4.1 to test iOS 15 and use the right iOS 15 version
    
    Remove link to older simulators from GitHub Action config
    
    Remove BuildKite artifact download step

--- apps/DemoApp/run-tests-big-sur.sh
+++ apps/DemoApp/run-tests-big-sur.sh
@@ -3,27 +3,20 @@ set -euo pipefail
 
 brew install xcbeautify
 
+echo "Running unit tests on Xcode version: $(xcode-select -p)"
+
 # Delete the old stuff
 rm -Rf XCFramework
 # reset simulators
 xcrun -v simctl shutdown all
 xcrun -v simctl erase all
 
-# Fetch artifact if running via buildkite (GitHub Actions fetches the artifact in a prior step)
-if command -v buildkite-agent > /dev/null 2>&1;
-then
-    buildkite-agent artifact download "MUXSDKStats.xcframework.zip" . --step ".buildkite/build.sh"
-fi
-
-echo "========= test xcode version"
-xcodebuild -version
-
 unzip MUXSDKStats.xcframework.zip
 cd apps/DemoApp
 pod deintegrate && pod update
 xcodebuild -workspace DemoApp.xcworkspace \
            -scheme "DemoApp" \
-           -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.2' \
+           -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.5' \
            test \
            | xcbeautify
 
