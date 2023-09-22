f5f0a81d4aaff873ccbc21956b7f357c0fb22985
AJ Lauer Barinov <abarinov@mux.com>
2023-09-21 19:51:21 -0700

    test: add SPM example application

---
+++ apps/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPM/BasicPlaybackExampleViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  MUXSDKStatsExampleSPM
+//
+
+import UIKit
+import AVKit
+import MUXSDKStats
+
+class BasicPlaybackExampleViewController: UIViewController {
+
+    var playbackURL: URL {
+        let playbackID = ProcessInfo.processInfo.playbackID ?? "qxb01i6T202018GFS02vp9RIe01icTcDCjVzQpmaB00CUisJ4"
+
+        return URL(
+            string: "https://stream.mux.com/\(playbackID).m3u8"
+        )!
+    }
+    let playerName = "AVPlayerViewControllerExample"
+    lazy var playerViewController = AVPlayerViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let playerData = MUXSDKCustomerPlayerData()
+        playerData.environmentKey = ProcessInfo.processInfo.environmentKey
+
+        let videoData = MUXSDKCustomerVideoData()
+        videoData.videoId = "VideoBehindTheScenes"
+        videoData.videoTitle = "Video Behind the Scenes"
+
+        let customerData = MUXSDKCustomerData(
+            customerPlayerData: playerData,
+            videoData: videoData,
+            viewData: nil
+        )
+
+        let player = AVPlayer(url: playbackURL)
+        playerViewController.player = player
+        playerViewController.delegate = self
+        playerViewController.allowsPictureInPicturePlayback = false
+
+        displayPlayerViewController()
+
+        MUXSDKStats.monitorAVPlayerViewController(
+            playerViewController,
+            withPlayerName: playerName,
+            customerData: customerData!
+        )
+    }
+
+    func displayPlayerViewController() {
+        self.addChild(playerViewController)
+        playerViewController.view
+            .translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(playerViewController.view)
+        self.view.addConstraints([
+            self.view.topAnchor.constraint(
+                equalTo: playerViewController.view.topAnchor
+            ),
+            self.view.bottomAnchor.constraint(
+                equalTo: playerViewController.view.bottomAnchor
+            ),
+            self.view.leadingAnchor.constraint(
+                equalTo: playerViewController.view.leadingAnchor
+            ),
+            self.view.trailingAnchor.constraint(
+                equalTo: playerViewController.view.trailingAnchor
+            )
+        ])
+        playerViewController.didMove(toParent:self)
+    }
+
+    func hidePlayerViewController() {
+        playerViewController.willMove(toParent: nil)
+        playerViewController.view.removeFromSuperview()
+        playerViewController.removeFromParent()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        playerViewController.player?.play()
+
+        guard let scene = UIApplication.shared.connectedScenes.first else {
+            return
+        }
+
+        if let sceneDelegate = scene.delegate as? SceneDelegate {
+            sceneDelegate.videoViewController = self
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        if !playerViewController.allowsPictureInPicturePlayback {
+            playerViewController.player?.pause()
+
+            MUXSDKStats.destroyPlayer(
+                playerName
+            )
+        }
+
+        super.viewWillDisappear(animated)
+    }
+}
+
+extension BasicPlaybackExampleViewController: AVPlayerViewControllerDelegate {
+    func playerViewControllerWillStartPictureInPicture(
+        _ playerViewController: AVPlayerViewController
+    ) {
+        guard let scene = UIApplication.shared.connectedScenes.first else {
+            return
+        }
+
+        if let sceneDelegate = scene.delegate as? SceneDelegate {
+            sceneDelegate.enteringPictureInPicture = true
+        }
+    }
+}
+
+
---
+++ apps/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPM/PlayerLayerExampleViewController.swift
@@ -0,0 +1,100 @@
+//
+//  PlayerLayerExampleViewController.swift
+//  MUXSDKStatsExampleSPM
+//
+
+import UIKit
+import MUXSDKStats
+
+/// UIView container for AVPlayerLayer
+class PlayerView: UIView {
+    override class var layerClass: AnyClass {
+        AVPlayerLayer.self
+    }
+
+    var player: AVPlayer? {
+        get {
+            (layer as? AVPlayerLayer)?.player
+        }
+        set {
+            (layer as? AVPlayerLayer)?.player = newValue
+        }
+    }
+}
+
+/// Bare bones AVPlayerLayer example without controls or
+/// other affordances
+class PlayerLayerExampleViewController: UIViewController {
+    
+    var playbackURL: URL {
+        let playbackID = ProcessInfo.processInfo.playbackID ?? "qxb01i6T202018GFS02vp9RIe01icTcDCjVzQpmaB00CUisJ4"
+
+        return URL(
+            string: "https://stream.mux.com/\(playbackID).m3u8"
+        )!
+    }
+    let playerName = "AVPlayerLayerExample"
+
+    lazy var playerView = PlayerView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let playerData = MUXSDKCustomerPlayerData()
+        playerData.environmentKey = ProcessInfo.processInfo.environmentKey
+
+        let videoData = MUXSDKCustomerVideoData()
+        videoData.videoId = "VideoBehindTheScenes"
+        videoData.videoTitle = "Video Behind the Scenes"
+
+        let customerData = MUXSDKCustomerData(
+            customerPlayerData: playerData,
+            videoData: videoData,
+            viewData: nil
+        )!
+
+        playerView.backgroundColor = .black
+
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(playerView)
+        view.addConstraints([
+            view.leadingAnchor.constraint(
+                equalTo: playerView.leadingAnchor
+            ),
+            view.trailingAnchor.constraint(
+                equalTo: playerView.trailingAnchor
+            ),
+            view.topAnchor.constraint(
+                equalTo: playerView.topAnchor
+            ),
+            view.bottomAnchor.constraint(
+                equalTo: playerView.bottomAnchor
+            ),
+        ])
+
+        playerView.player = AVPlayer(
+            url: playbackURL
+        )
+
+        guard let playerLayer = playerView.layer as? AVPlayerLayer else {
+            return
+        }
+
+        MUXSDKStats.monitorAVPlayerLayer(
+            playerLayer,
+            withPlayerName: playerName,
+            customerData: customerData
+        )
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playerView.player?.play()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        MUXSDKStats.destroyPlayer(playerName)
+        playerView.player?.pause()
+        super.viewWillDisappear(animated)
+    }
+}
