b22679b4d022842573375456d140449c2b353666
AJ Lauer Barinov <abarinov@mux.com>
2023-09-21 21:41:50 -0700

    build: maintain current minimum deployment target

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "MUXSDKStats",
     platforms: [
-        .iOS(.v12),
-        .tvOS(.v12)
+        .iOS(.v11),
+        .tvOS(.v11)
     ],
     products: [
         .library(name: "MUXSDKStats", targets: ["MUXSDKStatsTargets"])
