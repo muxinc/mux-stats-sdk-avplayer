09005e0c2302d9d4f3978d695faa1180b2b78736
AJ Lauer Barinov <abarinov@mux.com>
2023-09-25 10:00:42 -0700

    ci: run pod lib lint on release branches

---
+++ scripts/validate-podspec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "▸ Use Xcode 14.2"
+
+sudo xcode-select -switch /Applications/Xcode_14.2.app
+
+echo "▸ Current Xcode: $(xcode-select --print-path)"
+
+echo "▸ Using Xcode Version: $(xcodebuild -version | grep Xcode | cut -d " " -f2)"
+
+echo "▸ Validating Podspec"
+
+pod lib lint --allow-warnings --verbose
+
+echo "▸ Back to Xcode 14.3.1"
+
+sudo xcode-select -switch /Applications/Xcode_14.3.1.app
