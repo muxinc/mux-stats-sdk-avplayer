9fa2d96f7f369549e58215610a443d7d46d06f15
AJ Lauer Barinov <abarinov@mux.com>
2023-09-25 13:52:42 -0700

    ci: add barebones SPM example test
    intended to test SPM installations

---
+++ scripts/run-tests-spm-ventura.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+
+readonly XCODE=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
+
+if ! command -v xcbeautify &> /dev/null
+then
+	echo -e "\033[1;31m ERROR: xcbeautify could not be found please install it... \033[0m"
+    exit 1
+fi
+
+cd apps/MUXSDKStatsExampleSPM
+
+echo "▸ Current Xcode: $(xcode-select -p)"
+
+echo "▸ Using Xcode Version: ${XCODE}"
+
+echo "▸ Available Xcode SDKs"
+xcodebuild -showsdks
+
+echo "▸ Available Schemes in $(pwd)"
+xcodebuild -list -json
+
+echo "▸ Resolving SPM Dependencies"
+
+xcodebuild -resolvePackageDependencies \
+		   -project MUXSDKStatsExampleSPM.xcodeproj | xcbeautify
+
+echo "▸ Running Swift Package Manager Tests"
+echo ""
+echo "▸ Testing SDK on iOS 16.4 - iPhone 14 Pro Max"
+
+xcodebuild clean test \
+    -project MUXSDKStatsExampleSPM.xcodeproj \
+    -scheme "MUXSDKStatsExampleSPM" \
+    -destination 'platform=iOS Simulator,OS=16.4,name=iPhone 14 Pro Max' | xcbeautify
+
+echo "▸ Testing Mac Catalyst - Designed for iPad variant"
+
+xcodebuild clean test \
+	-project MUXSDKStatsExampleSPM.xcodeproj \
+	-scheme "MUXSDKStatsExampleSPM" \
+	-destination 'generic/platform=macOS,arch=arm64,variant=Designed for iPad' | xcbeautify
+
+
+
