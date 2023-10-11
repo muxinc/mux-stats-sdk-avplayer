1631a3c7d7157921b0bc8d24c5af928b74dac4ed
AJ Lauer Barinov <abarinov@mux.com>
2023-10-11 12:08:07 -0700

    docs: add audio-only playback example

---
+++ apps/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPM/AudioOnlyPlaybackExampleViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AudioOnlyPlaybackExampleViewController.swift
+//  MUXSDKStatsExampleSPM
+//
+
+import AVFoundation
+import UIKit
+
+import MuxCore
+import MUXSDKStats
+
+class AudioOnlyPlaybackExampleViewController: UIViewController {
+    var playbackURL: URL {
+        let playbackID = ProcessInfo.processInfo.playbackID ?? "27BKMLqT01tOznamh45ntvWXg00eZBRq3IFLTHX2T1rbY"
+
+        return URL(
+            string: "https://stream.mux.com/\(playbackID).m3u8"
+        )!
+    }
+    let playerName = "AudioOnlyPlaybackExample"
+    lazy var player = AVPlayer(url: playbackURL)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let playerData = MUXSDKCustomerPlayerData()
+        playerData.environmentKey = ProcessInfo.processInfo.environmentKey
+
+        let videoData = MUXSDKCustomerVideoData()
+        videoData.videoId = "FieldRecordingARainySummerNight"
+        videoData.videoTitle = "Field Recording: A Rainy Summer Night"
+
+        let customerData = MUXSDKCustomerData(
+            customerPlayerData: playerData,
+            videoData: videoData,
+            viewData: nil
+        )!
+
+        MUXSDKStats.monitorAVPlayer(
+            player,
+            withPlayerName: playerName, 
+            fixedPlayerSize: .zero,
+            customerData: customerData
+        )
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        player.play()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        MUXSDKStats.destroyPlayer(playerName)
+        player.pause()
+        super.viewWillDisappear(animated)
+    }
+}
