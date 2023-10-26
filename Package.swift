f60a47d167c4c818fe2dc6a9870a64da0d3751b0
AJ Lauer Barinov <abarinov@mux.com>
2023-10-26 05:35:18 -0700

    build: update manifest

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(
             name: "MuxCore",
             url: "https://github.com/muxinc/stats-sdk-objc.git",
-            from: "4.5.2"
+            from: "4.6.0"
         )
     ],
     targets: [
