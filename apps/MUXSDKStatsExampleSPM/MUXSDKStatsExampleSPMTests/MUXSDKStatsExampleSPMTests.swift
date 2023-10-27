27b7dae35b6778121a97cb01f057721acc6ff935
AJ Lauer Barinov <abarinov@mux.com>
2023-10-27 16:00:14 -0700

    remove unused test case

--- apps/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPMTests/MUXSDKStatsExampleSPMTests.swift
+++ apps/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPMTests/MUXSDKStatsExampleSPMTests.swift
@@ -5,14 +5,3 @@
 
 import XCTest
 @testable import MUXSDKStatsExampleSPM
-
-final class MUXSDKStatsExampleSPMTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-}
