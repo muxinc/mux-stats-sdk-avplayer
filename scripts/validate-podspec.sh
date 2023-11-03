8f20dd50c5e2afd2e1c28544e209cd7788acfa0c
AJ Lauer Barinov <abarinov@mux.com>
2023-11-03 12:47:48 -0700

    ci: validate pod spec using Xcode 15

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "▸ Use Xcode 14.2"
-
-sudo xcode-select -switch /Applications/Xcode_14.2.app
-
 echo "▸ Current Xcode: $(xcode-select --print-path)"
 
 echo "▸ Using Xcode Version: $(xcodebuild -version | grep Xcode | cut -d " " -f2)"
@@ -14,5 +10,3 @@ echo "▸ Validating Podspec"
 pod lib lint --allow-warnings --verbose
 
 echo "▸ Back to Xcode 14.3.1"
-
-sudo xcode-select -switch /Applications/Xcode_14.3.1.app/Contents/Developer
