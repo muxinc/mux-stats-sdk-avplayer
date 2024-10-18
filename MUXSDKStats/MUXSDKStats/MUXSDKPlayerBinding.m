764f03ca86e7537bda45390dc5b1a528fb4a3976
AJ Lauer Barinov <abarinov@mux.com>
2024-10-18 13:57:05 -0700

    Revert "fix: tear down view on hard application termination"
    This reverts commit e29c0a5065b610f87f0a1a8127f9845c506d8139.

--- MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
+++ MUXSDKStats/MUXSDKStats/MUXSDKPlayerBinding.m
@@ -142,11 +142,7 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleRenditionChange:) name:RenditionChangeNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAVPlayerError:) name:AVPlayerItemNewErrorLogEntryNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleConnectionTypeDetected:) name:@"com.mux.connection-type-detected" object:nil];
-
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(handleApplicationWillTerminate:)
-                                                 name:UIApplicationWillTerminateNotification
-                                               object:nil];
+    
     //
     // dylanjhaveri
     // See MUXSDKConnection.m for the tvos shortcoming
@@ -188,13 +184,6 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
             [MUXSDKCore dispatchGlobalDataEvent:dataEvent];
         }
     });
-}
-
-- (void)handleApplicationWillTerminate:(NSNotification *)notification {
-    [self dispatchViewEnd];
-    [self stopMonitoringAVPlayerItem];
-
-    [MUXSDKCore destroyPlayer:self.name];
 }
 
 # pragma mark AVPlayerItemAccessLog
@@ -402,7 +391,7 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
         if (_didTriggerManualVideoChange) {
             _didTriggerManualVideoChange = false;
         }
-        
+//        NSLog(@"%s: Dispatching View End", __PRETTY_FUNCTION__);
         [self dispatchViewEnd];
         [self stopMonitoringAVPlayerItem];
         
@@ -482,6 +471,7 @@ NSString * RemoveObserverExceptionName = @"NSRangeException";
     [self safelyRemovePlayerItemObserverForKeyPath:@"playbackBufferEmpty"];
     _playerItem = nil;
     if (!_isAdPlaying) {
+//        NSLog(@"%s: MUXSDKCore destroyPlayer", __PRETTY_FUNCTION__);
         [MUXSDKCore destroyPlayer: _name];
     }
 }
