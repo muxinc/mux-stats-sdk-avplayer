35f216129e83d320cb06eb3b554adfe8cbf18834
Kevin Cassidy Jr <kcassidy@mux.com>
2025-03-07 17:28:23 -0700

    remove test spec from podspec

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -8,9 +8,14 @@ echo "▸ Using Xcode Version: $(xcodebuild -version | grep Xcode | cut -d " " -
 echo "▸ Set US UTF-8 Locale"
 export LC_ALL=en_US.UTF-8
 
-echo "▸ Validating Podspec"
+echo "--- Linting Podspec (default options)"
+pod lib lint --skip-tests
 
-pod lib lint --skip-tests --verbose
-pod lib lint --skip-tests --use-libraries --verbose
-pod lib lint --skip-tests --use-modular-headers --verbose
-pod lib lint --skip-tests --use-static-frameworks --verbose
+echo "--- Linting Podspec (using libraries)"
+pod lib lint --skip-tests --use-libraries
+
+echo "--- Linting Podspec (using libraries and modular headers)"
+pod lib lint --skip-tests --use-libraries --use-modular-headers
+
+echo "--- Linting Podspec (using static frameworks)"
+pod lib lint --skip-tests --use-static-frameworks
