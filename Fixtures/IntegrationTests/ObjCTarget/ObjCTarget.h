9d5f0d44cd71298cdaeb7249c0298336490aa714
Kevin Cassidy Jr <kcassidy@mux.com>
2025-04-23 13:03:29 -0700

    Add a dummy ObjC target to integration tests project

---
+++ Fixtures/IntegrationTests/ObjCTarget/ObjCTarget.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjCTarget.
+FOUNDATION_EXPORT double ObjCTargetVersionNumber;
+
+//! Project version string for ObjCTarget.
+FOUNDATION_EXPORT const unsigned char ObjCTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCTarget/PublicHeader.h>
