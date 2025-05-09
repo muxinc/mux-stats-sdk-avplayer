5eae8626cf974bd3987f6e5afd83598d86855ec0
spuppo-mux <spuppo@mux.com>
2025-05-09 14:50:41 -0300

    Added configuration for Sauce Labs integration (#275)
    Added initial Sauce labs configuration to run tests from BuildKite
    Added UnitTests project to include unit tests target
    Added new Test Plan for Sauce Labs
    Changed projects configuration to match original IntegrationTests project config
    
    ---------
    
    Co-authored-by: Kevin Cassidy Jr <kcassidy@mux.com>
    Co-authored-by: Emily Dixon <edixon@mux.com>

---
+++ scripts/run-ci-tests.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -euo pipefail
+
+# set -x
+
+readonly WORKSPACE_PATH="$PWD/Fixtures/IntegrationTests/IntegrationTests.xcworkspace"
+readonly SCHEME=MUXSDKStats
+readonly TEST_PLAN=CIPipeline
+
+readonly BUILD_DIR="$PWD/.build"
+readonly ARTIFACTS_DIR="$BUILD_DIR/artifacts"
+readonly DERIVED_DATA_PATH="$BUILD_DIR/DerivedData"
+
+# re-exported so saucectl CLI can use them
+if [ "${CI:-}" ]; then
+    export SAUCE_USERNAME=$BUILDKITE_MAC_STADIUM_SAUCE_USERNAME
+    export SAUCE_ACCESS_KEY=$BUILDKITE_MAC_STADIUM_SAUCE_ACCESS_KEY
+fi
+
+# Prepare:
+
+EXIT_CODE=0
+
+mkdir -p "$BUILD_DIR" "$ARTIFACTS_DIR"
+
+if [ "${CI:-}" ]; then
+    (cd Configuration && ln -sF CodeSigning.mux.xcconfig CodeSigning.local.xcconfig)
+fi
+
+function test_for {
+    local platform="$1"
+    local destination_name="${2:-}"
+
+    echo "--- Building tests for $platform"
+
+    local safe_platform="${platform//[^[:alnum:]]/_}"
+    local test_products_filename="$SCHEME-$safe_platform.xctestproducts"
+    local test_products_path="$BUILD_DIR/$test_products_filename"
+
+    rm -rf "$test_products_path"
+
+    # Do not specialize the build (use generic platform)
+    set +e
+    xcodebuild clean build-for-testing \
+        -workspace "$WORKSPACE_PATH" \
+        -scheme "$SCHEME" \
+        -testPlan "$TEST_PLAN" \
+        -testProductsPath "$test_products_path" \
+        -destination "generic/platform=$platform" \
+        -derivedDataPath "$DERIVED_DATA_PATH" \
+        -allowProvisioningUpdates \
+        -disableAutomaticPackageResolution \
+        | xcbeautify
+    set -e
+    local xcodebuild_build_exit_code="$?"
+
+    if [ "$xcodebuild_build_exit_code" -ne 0 ]; then
+        echo "^^^ +++"
+        echo "xcodebuild exited with code $xcodebuild_build_exit_code"
+        EXIT_CODE=1
+        return
+    fi
+}
+
+function run_ci_tests {
+    echo "--- Running Sauce Labs Tests"
+
+    saucectl run
+}
+
+# Execute:
+
+test_for 'iOS'
+
+run_ci_tests
+
+exit "$EXIT_CODE"
