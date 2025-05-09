ea0c91b02e9b8eff5dc99f0ae8779c1a2ea7f703
Fabrizio-rar <fpersichetti17@gmail.com>
2025-05-09 15:35:02 -0300

    fixed referencing issues

---
+++ Fixtures/IntegrationTests/MUXSDKCore+Mock.h
@@ -0,0 +1,25 @@
+//
+//  MUXSDKCore+Mock.h
+//  MUXSDKStatsTests
+//
+//  Created by Nidhi Kulkarni on 2/4/20.
+//  Copyright © 2020 Mux, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MuxCore/MuxCore.h>
+
+@interface MUXSDKCore (Mock)
+
++ (void) swizzleDispatchEvents;
++ (void) resetCapturedEvents;
++ (id<MUXSDKEventTyping>) eventAtIndex:(NSUInteger) index forPlayer:(NSString *)playerId;
++ (NSUInteger) eventsCountForPlayer:(NSString *)playerId;
++ (MUXSDKDataEvent *) globalEventAtIndex:(NSUInteger)index;
++ (NSUInteger) globalEventsCount;
++ (NSArray<MUXSDKDataEvent *> *) snapshotOfGlobalEvents;
++ (NSArray<MUXSDKDataEvent *> *) snapshotOfEventsForPlayer:(NSString *)playerId;
++ (NSArray<MUXSDKBaseEvent *> *) getEventsForPlayer: (NSString *)playerId;
+
+@end
+
---
+++ Fixtures/IntegrationTests/MUXSDKCore+Mock.m
@@ -0,0 +1,105 @@
+//
+//  MUXSDKCore+Mock.m
+//  MUXSDKStatsTests
+//
+//  Created by Nidhi Kulkarni on 2/4/20.
+//  Copyright © 2020 Mux, Inc. All rights reserved.
+//
+
+#import "MUXSDKCore+Mock.h"
+#include <objc/runtime.h>
+
+
+@implementation MUXSDKCore (Mock)
+
+static NSMutableDictionary *events;
+static NSMutableArray *globalEvents;
+
++ (void) swizzleDispatchEvents {
+    static dispatch_once_t once_token;
+    dispatch_once(&once_token,  ^{
+        [MUXSDKCore _swizzle];
+    });
+}
+
++ (void) _swizzle {
+    [self _swizzleEvent];
+    [self _swizzleGlobalEvent];
+}
+
++ (void) _swizzleEvent {
+    SEL dispatchEventsSelector = @selector(dispatchEvent:forPlayer:);
+    SEL dispatchEventsMockSelector = @selector(mock_dispatchEvent:forPlayer:);
+
+    Method originalMethod = class_getClassMethod(self, dispatchEventsSelector);
+    Method extendedMethod = class_getClassMethod(self, dispatchEventsMockSelector);
+    method_exchangeImplementations(originalMethod, extendedMethod);
+}
+
++ (void) _swizzleGlobalEvent {
+    SEL dispatchGlobalEventsSelector = @selector(dispatchGlobalDataEvent:);
+    SEL dispatchGlobalEventsMockSelector = @selector(mock_dispatchGlobalDataEvent:);
+
+    Method originalMethod = class_getClassMethod(self, dispatchGlobalEventsSelector);
+    Method extendedMethod = class_getClassMethod(self, dispatchGlobalEventsMockSelector);
+    method_exchangeImplementations(originalMethod, extendedMethod);
+}
+
++ (void) resetCapturedEvents {
+    events = [[NSMutableDictionary alloc] init];
+    globalEvents = [[NSMutableArray alloc] init];
+}
+
++ (void) mock_dispatchGlobalDataEvent:(MUXSDKDataEvent *)event {
+    [globalEvents addObject:event];
+}
+
++ (void) mock_dispatchEvent:(id<MUXSDKEventTyping>)event forPlayer:(NSString *)playerId {
+    if (![events objectForKey:playerId]) {
+        [events setObject:[[NSMutableArray alloc] init] forKey:playerId];
+    }
+    NSMutableArray *eventsForPlayer = [events objectForKey:playerId];
+    [eventsForPlayer addObject:event];
+}
+
++ (id<MUXSDKEventTyping>) eventAtIndex:(NSUInteger) index forPlayer:(NSString *)playerId {
+    NSMutableArray *eventsForPlayer = [events objectForKey:playerId];
+    if(!eventsForPlayer) {
+        return nil;
+    }
+    return [eventsForPlayer objectAtIndex:index];
+}
+
++ (NSArray<MUXSDKBaseEvent *> *) getEventsForPlayer: (NSString *)playerId {
+    NSMutableArray *eventsForPlayer = [events objectForKey:playerId];
+    if(!eventsForPlayer) {
+        return nil;
+    }
+    return eventsForPlayer;
+}
+
++ (NSUInteger) eventsCountForPlayer:(NSString *)playerId {
+    NSMutableArray *eventsForPlayer = [events objectForKey:playerId];
+    if(!eventsForPlayer) {
+        return 0;
+    }
+    return eventsForPlayer.count;
+}
+
++ (MUXSDKDataEvent *) globalEventAtIndex:(NSUInteger)index {
+    return [globalEvents objectAtIndex:index];
+}
+
++ (NSUInteger) globalEventsCount {
+    return globalEvents.count;
+}
+
++ (NSArray<MUXSDKDataEvent *> *) snapshotOfGlobalEvents {
+    return [NSArray arrayWithArray:globalEvents];
+}
+
++ (NSArray<MUXSDKDataEvent *> *) snapshotOfEventsForPlayer:(NSString *)playerId {
+    return [NSArray arrayWithArray:[events objectForKey:playerId]];
+}
+
+@end
