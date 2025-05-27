872ffc9daa624520ed0841a9123f04521bd61e15
Kevin Cassidy Jr <kcassidy@mux.com>
2025-05-27 11:30:40 -0700

    remove unused method for now

--- Sources/MUXSDKStatsInternal/MUXSDKVideoData.swift
+++ Sources/MUXSDKStatsInternal/MUXSDKVideoData.swift
@@ -2,14 +2,6 @@ import AVFoundation
 import MuxCore
 
 extension MUXSDKVideoData {
-    func updateWithAssetDuration(_ duration: CMTime) {
-        let isLive = duration.isIndefinite
-        videoSourceIsLive = isLive ? "true" : "false"
-        if !isLive && !duration.seconds.isZero {
-            videoSourceDuration = duration.seconds as NSNumber
-        }
-    }
-
     func updateWithPresentationSize(_ presentationSize: CGSize) {
         videoSourceHeight = presentationSize.height as NSNumber
         videoSourceWidth = presentationSize.width as NSNumber
