4e066a51e5cc4ae6eafe801989f8b8ca09f54fa5
Emily Dixon <edixon@mux.com>
2025-05-29 17:58:51 -0700

    Tried an audio-only asset

--- Examples/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPM/BasicPlaybackExampleViewController.swift
+++ Examples/MUXSDKStatsExampleSPM/MUXSDKStatsExampleSPM/BasicPlaybackExampleViewController.swift
@@ -10,14 +10,16 @@ import MUXSDKStats
 class BasicPlaybackExampleViewController: UIViewController {
 
     var playbackURL: URL {
-        // normal(muxed) HLS asset
+        // normal(non-cmaf) HLS asset
         //        let playbackID = ProcessInfo.processInfo.playbackID ?? "qxb01i6T202018GFS02vp9RIe01icTcDCjVzQpmaB00CUisJ4"
 //        return URL(
 //            string: "https://stream.mux.com/\(playbackID).m3u8"
 //        )!
         
         // CMAF asset
-        URL("https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")!
+//        URL("https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")!
+        // Audio-only asset
+        URL("https://stream.mux.com/MwUGUc7gWwcE6AN6qVcilQ8cR4SFlE601kB96IiYqPVM.m3u8")!
     }
     let playerName = "AVPlayerViewControllerExample"
     lazy var playerViewController = AVPlayerViewController()
@@ -76,11 +78,13 @@ class BasicPlaybackExampleViewController: UIViewController {
 //                    print("segment request url: \(segmentEvent.url?.absoluteString ?? "nil")")
                     let urlFromPlaylistEvent =  segmentEvent.url
                     let urlFromNetworkTransactionMetrics = segmentEvent.mediaResourceRequestEvent?.networkTransactionMetrics?.transactionMetrics.last?.request.url
+                    let responseFromNetworkTransactionMetrics = segmentEvent.mediaResourceRequestEvent?.networkTransactionMetrics?.transactionMetrics.last?.response as! HTTPURLResponse
                     let urlFromResourceRequest = segmentEvent.mediaResourceRequestEvent!.url
                     print("segment request url from event: \(urlFromPlaylistEvent?.absoluteString ?? "nil")")
                     print("segment request url from Transaction metrics: \(urlFromNetworkTransactionMetrics?.absoluteString ?? "nil")")
                     print("segment request url resourceRequestEvent: \(urlFromResourceRequest?.absoluteString ?? "nil")")
-                    print("segment request media type: \(segmentEvent.mediaType.rawValue)")
+                    // the mediaResoureRequestEvent.mediaType is .video for Video segments and .audio for Audio Segments
+                    print("segment MediaResourceRequest mediaType: \(segmentEvent.mediaType.rawValue)")
                     let mediaTypeFromSwitch: String
                     switch segmentEvent.mediaType {
                         case .audio:
@@ -88,9 +92,15 @@ class BasicPlaybackExampleViewController: UIViewController {
                     case .video:
                         mediaTypeFromSwitch = "video"
                     default:
-                        mediaTypeFromSwitch = "not audio or video"
-                    }
-                    print("segment request media type (selected by switch statement): \(mediaTypeFromSwitch)")
+                        mediaTypeFromSwitch = "not audio or-video"
+                     }
+                    print("segment request media type (selected by switch-statement): \(mediaTypeFromSwitch)")
+                    
+                    // the mediaResourceRequestEvent.mediaType comes from logic internal to the player. It does not correlate to the Content-Type of the response
+                    //  (it's all a black box, but it could either come from the MVP (EXT-X-MEDIA:TYPE) or could be based on what's discovered when
+                    //   demuxing the segment.. For sure it's not based off of content-type header, which you can tell by playing an audio-only mux asset
+                    //   and observing that the Content-Type is 'video/MP2T' but the AVMediaType is correctly set to .audio)
+                    print("segment response content-type: \(String(describing: responseFromNetworkTransactionMetrics.allHeaderFields["Content-Type"]))")
                     print("")
                 }
             }
@@ -111,15 +121,6 @@ class BasicPlaybackExampleViewController: UIViewController {
                 }
             }
         }
-        
-//        while true {
-//            let event = try await it.next()
-//            print("got one: \(String(describing: event))")
-            
-//            let resourceEvent = try await resourceRequests.next()
-//            guard let resourceEvent else { return }
-//            print("segment request media type: \(resourceEvent.mediaType.rawValue)")
-//        }
     }
     
     func displayPlayerViewController() {
