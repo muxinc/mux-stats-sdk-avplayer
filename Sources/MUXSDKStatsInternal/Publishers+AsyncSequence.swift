70c8a8c67e960567cfa46e270c259b1ce8b4bdaa
Kevin Cassidy Jr <kcassidy@mux.com>
2025-06-02 11:41:45 -0700

    use typed failure as this is only for iOS 18+

--- Sources/MUXSDKStatsInternal/Publishers+AsyncSequence.swift
+++ Sources/MUXSDKStatsInternal/Publishers+AsyncSequence.swift
@@ -1,17 +1,17 @@
 import Combine
 
-@available(iOS 13, tvOS 13, *)
+@available(iOS 18, tvOS 18, *)
 extension AsyncSequence {
-    var publisher: some Publisher<Element, Error> {
-        let subject = PassthroughSubject<Element, Error>()
+    var publisher: some Publisher<Element, Failure> {
+        let subject = PassthroughSubject<Element, Failure>()
 
-        let task = Task {
-            do {
-                for try await event in self {
+        let task = Task<Void, Never> {
+            do throws(Failure) {
+                for try await element in self {
                     if Task.isCancelled {
                         break
                     }
-                    subject.send(event)
+                    subject.send(element)
                 }
                 subject.send(completion: .finished)
             } catch {
