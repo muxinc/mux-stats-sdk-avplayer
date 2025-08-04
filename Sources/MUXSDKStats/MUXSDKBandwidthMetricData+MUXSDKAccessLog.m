dfb563d1719e2f7de899d031f7d4b83ea840c5f6
Kevin Cassidy Jr <kcassidy@mux.com>
2025-08-04 11:42:40 -0700

    simplify access log uri handling, tests

--- Sources/MUXSDKStats/MUXSDKBandwidthMetricData+MUXSDKAccessLog.m
+++ Sources/MUXSDKStats/MUXSDKBandwidthMetricData+MUXSDKAccessLog.m
@@ -1,7 +1,5 @@
 #import "MUXSDKBandwidthMetricData+MUXSDKAccessLog.h"
 
-#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
-
 @implementation MUXSDKBandwidthMetricData (MUXSDKAccessLog)
 
 - (void)updateURLPropertiesAndRequestTypeWithRequestURI:(nullable NSString *)requestURI {
@@ -15,23 +13,27 @@
     // This is not always going to work, as it's perfectly in-spec to set content type headers instead.
     // Also the access log tends to only report playlist requests anyway. See AVMetrics for useful data.
     NSString *pathExtension = components.path.pathExtension;
-    if (@available(iOS 14.0, tvOS 14.0, *)) {
-        UTType *inferredType = [UTType typeWithFilenameExtension:pathExtension];
-        if ([inferredType conformsToType:UTTypeVideo]) {
-            self.requestType = @"video";
-        } else if ([inferredType conformsToType:UTTypeAudio]) {
-            self.requestType = @"audio";
-        } else if ([inferredType conformsToType:UTTypeAudiovisualContent]) {
-            self.requestType = @"media";
-        } else if ([inferredType conformsToType:UTTypePlaylist]) {
-            self.requestType = @"manifest";
-        }
+    if (pathExtension) {
+        static NSDictionary<NSString *, NSString *> *lookup;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            lookup = @{
+                @"m3u8": @"manifest",
+                @"m3u": @"manifest",
+
+                @"ts": @"media",
+                @"mp4": @"media",
+                @"mov": @"media",
+
+                @"aac": @"audio",
+                @"ac3": @"audio",
+                @"m4a": @"audio",
+                @"mp3": @"audio",
+            };
+        });
+        self.requestType = lookup[pathExtension];
     } else {
-        if ([pathExtension isEqual:@"m3u8"] || [pathExtension isEqual:@"m3u"]) {
-            self.requestType = @"manifest";
-        } else if ([pathExtension isEqual:@"ts"] || [pathExtension isEqual:@"mp4"]) {
-            self.requestType = @"media";
-        }
+        self.requestType = nil;
     }
 }
 
