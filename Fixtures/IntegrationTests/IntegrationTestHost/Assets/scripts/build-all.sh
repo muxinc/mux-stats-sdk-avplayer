7b4baf42feb5110f110fb8e64eba3c946e1df10d
ignasio-mux <iroure@mux.com>
2025-09-15 15:53:39 -0300

    remove build all

--- Fixtures/IntegrationTests/IntegrationTestHost/Assets/scripts/build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PACKAGE_ASSETS_DIR="../../../../Packages/IntegrationTestAssets/Sources/IntegrationTestAssets/assets"
-mkdir -p "$PACKAGE_ASSETS_DIR"
-export ASSETS_DIR="$PACKAGE_ASSETS_DIR"
-bash ./scripts/download-inputs.sh
-bash ./scripts/assets-make-segments.sh
-bash ./scripts/assets-make-variants.sh
-bash ./scripts/assets-make-cmaf.sh
-bash ./scripts/assets-make-encrypted.sh
