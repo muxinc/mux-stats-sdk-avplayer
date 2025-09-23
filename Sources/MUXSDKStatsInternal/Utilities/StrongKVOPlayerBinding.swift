f1aaf21ee87a6fed054dbfc9e8b58fca9bf8303a
Kevin Cassidy Jr <kcassidy@mux.com>
2025-09-23 15:14:41 -0700

    Sendability/MainActor

--- Sources/MUXSDKStatsInternal/Utilities/StrongKVOPlayerBinding.swift
+++ Sources/MUXSDKStatsInternal/Utilities/StrongKVOPlayerBinding.swift
@@ -2,28 +2,54 @@ import Foundation
 import AVKit
 
 @objc(MUXSDKStrongKVOPlayerVCBinding)
-public final class StrongKVOPlayerVCBinding: NSObject {
-    private var observation: NSKeyValueObservation?
-    @objc private(set) public var playerViewController: AVPlayerViewController?
-
+public final class StrongKVOPlayerVCBinding: NSObject, Sendable {
     @MainActor
-    @objc public init(playerViewController: AVPlayerViewController, onPlayerChange: @Sendable @escaping (AVPlayer?) -> Void) {
-        observation = playerViewController.observe(\.player, options: [.initial, .new]) { _, change in
-            onPlayerChange(change.newValue!)
-        }
+    private var observation: NSKeyValueObservation?
+
+    @objc public let playerViewController: AVPlayerViewController
+
+    @objc public init(playerViewController: AVPlayerViewController,
+                      onPlayerChange: @Sendable @escaping (AVPlayer?) -> Void) {
         self.playerViewController = playerViewController
+        super.init()
+
+        let startObserving = { @MainActor [weak self] in
+            guard let self else {
+                return
+            }
+            self.observation = playerViewController.observe(\.player, options: [.initial, .new]) { _, change in
+                onPlayerChange(change.newValue!)
+            }
+        }
+
+        guard #available(iOS 13, tvOS 13, *) else {
+            DispatchQueue.main.async(execute: startObserving)
+            return
+        }
+
+        if DispatchQueue.isMainQueue {
+            MainActor.assumeIsolated {
+                startObserving()
+            }
+        } else {
+            Task { @MainActor in
+                startObserving()
+            }
+        }
     }
 }
 
 @objc(MUXSDKStrongKVOPlayerLayerBinding)
 public final class StrongKVOPlayerLayerBinding: NSObject {
-    private var observation: NSKeyValueObservation?
-    @objc private(set) public var playerLayer: AVPlayerLayer?
+    private let observation: NSKeyValueObservation
 
-    @objc public init(playerLayer: AVPlayerLayer, onPlayerChange: @Sendable @escaping (AVPlayer?) -> Void) {
+    @objc public let playerLayer: AVPlayerLayer
+
+    @objc public init(playerLayer: AVPlayerLayer,
+                      onPlayerChange: @Sendable @escaping (AVPlayer?) -> Void) {
+        self.playerLayer = playerLayer
         observation = playerLayer.observe(\.player, options: [.initial, .new]) { _, change in
             onPlayerChange(change.newValue!)
         }
-        self.playerLayer = playerLayer
     }
 }
