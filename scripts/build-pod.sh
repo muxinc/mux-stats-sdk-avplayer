6c9df1dd85887f6b6353947baff8ba9b62d15afe
Kevin Cassidy Jr <kcassidy@mux.com>
2026-01-12 14:25:04 -0800

    Bump MuxCore version requirement

--- scripts/build-pod.sh
+++ scripts/build-pod.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Keep this at "~> X.Y" to match the behavior of the "from:" requirement in Package.swift
-readonly MUXCORE_VERSION="~> 5.7"
+readonly MUXCORE_VERSION="~> 5.8"
 
 readonly CODE_SIGNING_IDENTITY="Apple Distribution: Mux, Inc (XX95P4Y787)"
 
