4610fb0b99e94b293eb474fcfbb45d0ea8ecd054
Kevin Cassidy Jr <kcassidy@mux.com>
2026-03-10 13:24:53 -0400

    Fire an initial renditionchange event (#349)

--- Sources/MUXSDKStatsInternal/Utilities/Future+async.swift
+++ Sources/MUXSDKStatsInternal/Utilities/Future+async.swift
@@ -2,9 +2,9 @@ import Combine
 
 @available(iOS 13, tvOS 13, *)
 extension Future where Failure == Never {
-    convenience init(isolation: isolated (any Actor)? = #isolation, operation: @escaping () async -> sending Output) {
+    convenience init(isolation: isolated (any Actor)? = #isolation, priority: TaskPriority? = nil, operation: @escaping () async -> sending Output) {
         self.init { promise in
-            Task {
+            Task(priority: priority) {
                 _ = isolation
                 promise(.success(await operation()))
             }
