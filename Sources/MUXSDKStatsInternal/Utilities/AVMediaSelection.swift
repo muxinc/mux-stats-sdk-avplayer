f82f96ef8235b63619dc4e78fd2d403551f1afc7
Kevin Cassidy Jr <kcassidy@mux.com>
2026-04-01 13:31:48 -0700

    factor out metadata loading

--- Sources/MUXSDKStatsInternal/Utilities/AVMediaSelection.swift
+++ Sources/MUXSDKStatsInternal/Utilities/AVMediaSelection.swift
@@ -35,28 +35,19 @@ extension AVMediaSelectionOption {
 
     @MainActor
     func loadHLSNameAttributeValue() async -> String? {
-        let metadataItems = AVMetadataItem.metadataItems(
-            from: commonMetadata,
-            filteredByIdentifier: .hlsNameAttribute)
-
-        for item in metadataItems {
-            do {
-                if let stringValue = try await item.load(.stringValue) {
-                    return stringValue
-                }
-            } catch {
-                logger.debug("Failed to load metadata item \(item): \(error)")
-            }
-        }
-
-        return nil
+        await loadFirstStringValue(for: .hlsNameAttribute)
     }
 
     @MainActor
     func loadTitle() async -> String? {
+        await loadFirstStringValue(for: .commonIdentifierTitle)
+    }
+
+    @MainActor
+    func loadFirstStringValue(for identifier: AVMetadataIdentifier) async -> String? {
         let metadataItems = AVMetadataItem.metadataItems(
             from: commonMetadata,
-            filteredByIdentifier: .commonIdentifierTitle)
+            filteredByIdentifier: identifier)
 
         for item in metadataItems {
             do {
