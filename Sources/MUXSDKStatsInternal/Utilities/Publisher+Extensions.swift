1bd0e9412a0ea180204ebc02f4df24c5780913b3
Kevin Cassidy Jr <kcassidy@mux.com>
2026-04-13 17:03:18 -0700

    update Publisher async map extension

--- Sources/MUXSDKStatsInternal/Utilities/Publisher+Extensions.swift
+++ Sources/MUXSDKStatsInternal/Utilities/Publisher+Extensions.swift
@@ -2,8 +2,10 @@ import Combine
 
 @available(iOS 14, tvOS 14, *)
 extension Publisher {
+
+    /// Transforms all elements from the upstream publisher with a provided async closure, waiting for completion before advancing to the next element.
     func map<T>(isolation: isolated (any Actor)? = #isolation, priority: TaskPriority? = nil, _ transform: @escaping (Self.Output) async -> sending T) -> some Publisher<T, Failure> {
-        flatMap { value in
+        flatMap(maxPublishers: .max(1)) { value in
             Future(isolation: isolation, priority: priority) {
                 await transform(value)
             }
