2178de7977ac5303adbf83374b1d77d3e7bb759c
Kevin Cassidy Jr <kcassidy@mux.com>
2026-06-02 13:50:50 -0400

    revert me: proof this works

--- Package.swift
+++ Package.swift
@@ -9,8 +9,9 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/muxinc/stats-sdk-objc.git",
-            from: "5.11.0"),
+            name: "stats-sdk-objc",
+            url: "git@github.com:muxinc/mux-stats-sdk-objc.git",
+            branch: "master"),
     ],
     targets: [
         .target(
